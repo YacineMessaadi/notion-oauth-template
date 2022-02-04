c14d24306e6d363dce8acee13935c38295b1b838
YacineMessaadi <yacinemess00@gmail.com>
2022-02-04 10:47:56 +0100

    Add notion oauth get token

---
+++ pages/api/auth/notion.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default function handler(req, res) {
+  console.log(req.query);
+  //Exchange authorization code for access token
+  axios({
+    method: "post",
+    url: "https://api.notion.com/v1/oauth/token",
+    data: {
+      code: req.query.code,
+      grant_type: "authorization_code",
+      redirect_uri: "https://wrappr.dev",
+    },
+    auth: {
+      username: process.env.NOTION_CLIENT_ID,
+      password: process.env.NOTION_CLIENT_SECRET,
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      return res.status(200).json({ response });
+    })
+    .catch((err) => {
+      return res.status(500).json(err.message);
+    });
+}
